0a3e4c1dbec50abf5a1b7a3977f573d5d9eddb99
IVAM GALVAO FILHO <programador.ivam@gmail.com>
2021-11-08 10:03:36 -0300

    feat: new classes for classes-react

---
+++ App/Classes/ClassReact.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Car extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+}
+
+ReactDOM.render(<Car />, document.getElementById('root'));
